f614c8af1d2d3dac658add24ad45350784115cd6
ravindukrs <ravindupf@gmail.com>
2021-06-25 21:50:51 +0530

    Added CORS support

--- app.py
+++ app.py
@@ -2,18 +2,22 @@ from flask import Flask
 from flask import request, jsonify, Response
 import pickle
 import numpy as np
+from flask_cors import CORS, cross_origin
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import LabelEncoder
 
 
 app = Flask(__name__)
-
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/ping', methods=['GET'])
+@cross_origin()
 def ping():
     return Response(status=200);
 
 @app.route('/predict', methods=['POST'])
+@cross_origin()
 def predict():
     data = request.get_json()
     body = data.get("body")
@@ -26,9 +30,9 @@ def predict():
     mydata[:, 0] = le_X_0.transform(mydata[:, 0])
     mydata = scaler.transform(mydata)
     prediction = loaded_model.predict(mydata)
-    response = jsonify({'some': 'data'})
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return jsonify({"mileage":round(prediction[0])})
+    response = jsonify({"mileage":round(prediction[0])})
+    # response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 
 if __name__ == '__main__':
